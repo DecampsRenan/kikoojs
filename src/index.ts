9fea7a12298118cab01cafa6cc28045833c58040
Renan Decamps <renan.decamps@gmail.com>
2025-11-10 20:45:53 +0100

    feat: add easter-eggs-collection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
+"use client";
+
 export type { UseElevatorOptions } from "./elevator/useElevator";
 export { useElevator } from "./elevator/useElevator";
 
 export type { UseGlitchOptions } from "./useGlitch";
 export { useGlitch } from "./useGlitch";
+
+// auto-load this lib
+import "easter-egg-collection";
