02779b498739c82fb05638fc762927f089480012
Renan Decamps <renan.decamps@gmail.com>
2025-11-14 17:34:19 +0100

    aha remove the crap so it is easier for me

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,13 +1,5 @@
-import { useState } from "react";
 import { version } from "../../package.json";
-import {
-	useCursors,
-	useDvd,
-	useElevator,
-	useGlitch,
-	useKonamiCode,
-	useRainbow,
-} from "../../src";
+import { Title } from "./Title";
 
 export function App() {
 	// Glitch the title
@@ -27,29 +19,20 @@ export function App() {
 	return (
 		<div className="space-y-10 flex flex-col max-w-150 mx-auto mb-8 px-8">
 			<div className="flex flex-col items-center gap-8 h-screen pt-20">
-				{/** biome-ignore lint/a11y/useSemanticElements: I want it to be a div */}
-				<div
-					className="flex flex-col gap-1 justify-center"
-					tabIndex={0}
-					role="button"
-				>
+				<Title className="flex flex-col gap-1 justify-center">
 					<h1 className={`text-4xl md:text-5xl lg:text-7xl font-extrabold`}>
 						KikooJS
 					</h1>
 					<span className={`text-sm`}>v{version}</span>
-				</div>
+				</Title>
 				<div>
-					<p>
-						You never asked for it, now it’s real. All the fun libs in one
-						package ✨
-					</p>
+					You never asked for it, now it’s real. All the fun libs in one package
+					✨
 				</div>
 
 				<code className="bg-gray-100 rounded px-6 py-4 shadow-inner">
 					npm install kikoojs
 				</code>
-
-				<div className="flex-1" />
 			</div>
 		</div>
 	);
---
+++ playground/src/Title.tsx
@@ -0,0 +1,20 @@
+import type { ComponentProps } from "react";
+
+export const Title = ({
+	onClick,
+	...props
+}: Omit<ComponentProps<"div">, "onClick"> & { onClick?: () => void }) => {
+	return (
+		/** biome-ignore lint/a11y/useSemanticElements: I want it to be a div */
+		<div
+			tabIndex={0}
+			role="button"
+			{...props}
+			onKeyDown={(event) => {
+				if (!["Enter", " "].includes(event.key)) return;
+				props?.onKeyDown?.(event);
+				onClick?.();
+			}}
+		/>
+	);
+};
