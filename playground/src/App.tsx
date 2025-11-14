0332a7daa17379ecb45cceec53e1729443331287
Renan Decamps <renan.decamps@gmail.com>
2025-11-14 17:27:55 +0100

    remove all fancy effects

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -10,33 +10,19 @@ import {
 } from "../../src";
 
 export function App() {
-	const [hasTouchedCorner, setHasTouchedCorner] = useState(false);
-	const [titleRef] = useGlitch<HTMLHeadingElement>({ playMode: "hover" });
+	// Glitch the title
 
-	const [triggerEltRef] = useElevator<HTMLButtonElement>({
-		mainAudio: "/sfx/waiting.mp3",
-		endAudio: "/sfx/ding.mp3",
-	});
+	// Dvd Bounce
 
-	useCursors({
-		enabledCursors: ["fairyDustCursor", "trailingCursor", "emojiCursor"],
-		containerElement: document.body,
-	});
+	// Elevator
 
-	const [fullTitleRef, dvdControls] = useDvd<HTMLDivElement>({
-		xSpeed: 500,
-		ySpeed: 500,
-		onCorner: () => {
-			// trigger confettis and shake screen and animate page background
-			setHasTouchedCorner(true);
-		},
-	});
+	// Easter eggs
 
-	useRainbow<HTMLDivElement>({ enabled: hasTouchedCorner });
+	// Add fancy cursor effects
 
-	useKonamiCode({
-		onSuccess: () => setHasTouchedCorner(true),
-	});
+	// On dvd corner bounce, animate background
+
+	// Oh crap, I don't have time to wait => konami code to trigger background
 
 	return (
 		<div className="space-y-10 flex flex-col max-w-150 mx-auto mb-8 px-8">
@@ -44,30 +30,13 @@ export function App() {
 				{/** biome-ignore lint/a11y/useSemanticElements: I want it to be a div */}
 				<div
 					className="flex flex-col gap-1 justify-center"
-					ref={fullTitleRef}
 					tabIndex={0}
 					role="button"
-					onKeyDown={(event) => {
-						if (!["Enter", " "].includes(event.key)) return;
-						if (dvdControls.isPlaying) return;
-						dvdControls.start();
-					}}
-					onClick={() => {
-						if (dvdControls.isPlaying) return;
-						dvdControls.start();
-					}}
 				>
-					<h1
-						className={`text-4xl md:text-5xl lg:text-7xl font-extrabold ${dvdControls.isPlaying ? "" : "text-[#ff4894]"}`}
-						ref={titleRef}
-					>
+					<h1 className={`text-4xl md:text-5xl lg:text-7xl font-extrabold`}>
 						KikooJS
 					</h1>
-					<span
-						className={`text-sm ${dvdControls.isPlaying ? "" : "text-gray-500"}`}
-					>
-						v{version}
-					</span>
+					<span className={`text-sm`}>v{version}</span>
 				</div>
 				<div>
 					<p>
@@ -80,38 +49,8 @@ export function App() {
 					npm install kikoojs
 				</code>
 
-				<div>
-					<p>
-						Try typing "hi", "no", "homer", "bravo" or "lol" (there is many
-						more, all options not listed here)
-					</p>
-					<span className="text-xs">
-						Powered by{" "}
-						<a
-							className="underline underline-offset-2"
-							href="https://github.com/WeiChiaChang/easter-egg-collection"
-							target="_blank"
-							rel="noopener"
-						>
-							Easter Eggs Collection
-						</a>
-					</span>
-				</div>
-
 				<div className="flex-1" />
-
-				<div className="my-5">↓ Scroll down ↓</div>
 			</div>
-
-			<div className="h-1200" />
-
-			<button
-				ref={triggerEltRef}
-				className="bottom-5 right-5 bg-amber-200 text-amber-800 px-6 pt-4 pb-2 rounded font-bold cursor-pointer hover:shadow-2xl hover:scale-105 transition-all active:scale-95"
-				type="button"
-			>
-				ElevatorJS
-			</button>
 		</div>
 	);
 }
--- playground/src/index.tsx
+++ playground/src/index.tsx
@@ -6,7 +6,7 @@ import { createRoot } from "react-dom/client";
 
 import { App } from "./App.tsx";
 import "./style.css";
-import "../../src/easter-egg-collection-register.ts";
+// import "../../src/easter-egg-collection-register.ts";
 
 // biome-ignore lint/style/noNonNullAssertion: always defined here.
 createRoot(document.querySelector("#app")!).render(
